4d0f5f53d10034efe48455dbccad3cecc9f7bf87
AlySag12 <165717572+AlySag12@users.noreply.github.com>
2024-10-09 20:30:18 -0400

    Delete SQLQuerySupervisortable.sql

--- SQLQuerySupervisortable.sql
+++
@@ -1,9 +0,0 @@
-use workers
-
-create table supervisor(
-supervisorID int, firstname Varchar (50), lastname Varchar (50), email Varchar (50), divison Varchar (50)
-)
-insert into supervisor(supervisorID, firstname, lastname, email, divison)
-values(2617371, 'rebecca','james','rjames@email.com','DOC')
-
-Select * from supervisor
